217a3e341703154488a8b3c5be9dbd9f0d19e6fb
Luca <lucacuria@gmail.com>
2025-04-14 20:50:13 -0300

    desafio 2

---
+++ desafios/d1/main.js
@@ -0,0 +1,5 @@
+let nombreProducto=("Coca Cola");
+let cantidadDeseada=prompt("Cuántos productos de "+ nombreProducto +" desea comprar?");
+let precioUnitario=1000;
+const costoTotal=cantidadDeseada*precioUnitario;
+alert("El costo total de la compra es: $"+costoTotal);
