d6e98889e2968b4fa236da18b48878d13cc5af77
Luca <lucacuria@gmail.com>
2025-04-14 20:03:39 -0300

    seguir codigo main.js

--- assets/main.js
+++ assets/main.js
@@ -1,2 +1,13 @@
-alert("Alerta desde js");
-console.log("Hola desde main.js"); 
+/*alert("Alerta desde js");*/
+console.log("Hola desde main.js");
+const nombre_alumno="Luca";
+const apellido_alumno="Curia";
+const nombre_completo=nombre_alumno+" "+apellido_alumno;
+const nombre_mejorado=`${nombre_alumno} ${apellido_alumno}`;
+let edad=17
+console.log("Nombre completo: "+nombre_completo);
+console.log("Nombre mejorado: "+nombre_mejorado);
+console.log("Mi edad es: "+edad);
+
+let edad_usuario=prompt("¿Cuál es tu edad?");
+console.log("La edad del usuario es: "+edad_usuario);
