2a6bc080d8caf54a585c920d1f0d10c3589ecc6c
Luca <lucacuria@gmail.com>
2025-04-21 18:49:58 -0300

    desafio 3

---
+++ desafios/d3/main.js
@@ -0,0 +1,11 @@
+let nombreProducto=("Coca Cola");
+let cantidadDeseada=prompt("Cuántos productos de "+ nombreProducto +" desea comprar?");
+let precioUnitario=1000;
+const costoTotal=cantidadDeseada*precioUnitario;
+if (cantidadDeseada>=5){
+    const descuento=0.1*costoTotal;
+    alert(`El costo total de la compra con descuento es: $${costoTotal - descuento}`);
+} 
+else {
+    alert(`El costo total de la compra es: $${costoTotal}`);
+}
