0df95c9771ded392ee707ab5baf9ae195586a099
Luca <lucacuria@gmail.com>
2025-04-21 18:59:49 -0300

    desafio 3

--- desafios/d3/main.js
+++ desafios/d3/main.js
@@ -4,6 +4,7 @@ let precioUnitario=1000;
 const costoTotal=cantidadDeseada*precioUnitario;
 if (cantidadDeseada>=5){
     const descuento=0.1*costoTotal;
+    alert(`El costo total de la compra es: $${costoTotal} y el descuento es de : $${descuento}`);
     alert(`El costo total de la compra con descuento es: $${costoTotal - descuento}`);
 } 
 else {
