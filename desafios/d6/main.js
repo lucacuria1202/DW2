dee5360bc91579c6881d219f3e412d67957e8af6
Luca <lucacuria@gmail.com>
2025-05-12 19:43:12 -0300

    desafio 6

--- desafios/d6/main.js
+++ desafios/d6/main.js
@@ -1,8 +1,16 @@
-let nombreProducto=("Coca Cola");
-const cantidad=parseInt(prompt("Cuántos productos de "+ nombreProducto +" desea comprar?"));
-const precioUnitario=1000;
+const productos = [
+  {
+    nombre: "Camiseta",
+    precio: 1000,
+    cantidad: 6,
+  },
+];
+productos[0].nombre = "Zapatillas";
+productos[0].categoria = "Calzado";
+delete productos[0].cantidad;
 
-function sumarProductos(precioUnitario, cantidad) {
-  return precioUnitario * cantidad;
+for (let i = 0; i < productos.length; i++) {
+  console.log(`Nombre del producto: ${productos[i].nombre}`);
+  console.log(`Precio del producto: ${productos[i].precio}`);
+  console.log(`Categoría del producto: ${productos[i].categoria}`);
 }
-alert("El precio total de su compra es de: " +sumarProductos(precioUnitario, cantidad));
